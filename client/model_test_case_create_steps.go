5882395f8abc6ec1d937f720091cd91d03c0b945
Max <max.ross@suse.com>
2025-05-12 15:16:24 -0700

    Merge pull request #3 from rancher-max/test-steps
    Update test steps to use the correct value in the api

--- client/model_test_case_create_steps.go
+++ client/model_test_case_create_steps.go
@@ -3,7 +3,7 @@ package client
 type TestCaseCreateSteps struct {
 	Action         string   `json:"action,omitempty"`
 	ExpectedResult string   `json:"expected_result,omitempty"`
-	InputData      string   `json:"input_data,omitempty"`
+	Data           string   `json:"data,omitempty"`
 	Position       int32    `json:"position,omitempty"`
 	Attachments    []string `json:"attachments,omitempty"`
 }
--- client/model_test_case_update_steps.go
+++ client/model_test_case_update_steps.go
@@ -3,6 +3,6 @@ package client
 type TestCaseUpdateSteps struct {
 	Action         string `json:"action,omitempty"`
 	ExpectedResult string `json:"expected_result,omitempty"`
-	InputData      string `json:"input_data,omitempty"`
+	Data           string `json:"data,omitempty"`
 	Position       int32  `json:"position,omitempty"`
 }
